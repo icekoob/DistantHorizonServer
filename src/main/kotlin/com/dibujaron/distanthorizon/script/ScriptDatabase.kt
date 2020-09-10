8a0e68d4fff07f8281c74ba97cd460cc186d801a
Drew Polstra <dibujaron@gmail.com>
2020-09-09 21:20:14 -0400

    Partial Postgres implementation of Route, RouteStep

--- src/main/kotlin/com/dibujaron/distanthorizon/script/ScriptDatabase.kt
+++ src/main/kotlin/com/dibujaron/distanthorizon/script/ScriptDatabase.kt
@@ -2,10 +2,10 @@ package com.dibujaron.distanthorizon.script
 
 import com.dibujaron.distanthorizon.orbiter.Station
 
-interface ScriptDatabase {
-    fun selectStationsWithScripts(): List<Station>
-    fun selectAvailableScript(sourceStation: Station, targetStation: Station, earliestDepartureTick: Int, latestDepartureTick: Int): ScriptReader?
-    fun selectAvailableScriptToAnywhere(sourceStation: Station, earliestDepartureTick: Int, latestDepartureTick: Int): ScriptReader?
-    fun beginLoggingScript(sourceStation: Station): ScriptWriter
-    fun shutdown()
+abstract class ScriptDatabase {
+    abstract fun selectStationsWithScripts(): List<Station>
+    abstract fun selectAvailableScript(sourceStation: Station, targetStation: Station, earliestDepartureTick: Int, latestDepartureTick: Int): ScriptReader?
+    abstract fun selectAvailableScriptToAnywhere(sourceStation: Station, earliestDepartureTick: Int, latestDepartureTick: Int): ScriptReader?
+    abstract fun beginLoggingScript(sourceStation: Station): ScriptWriter
+    abstract fun shutdown()
 }
