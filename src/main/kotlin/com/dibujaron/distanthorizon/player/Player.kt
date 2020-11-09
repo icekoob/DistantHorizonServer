9e44e0b59d712ecd8e931944874d7d1e5cd5713f
Drew Polstra <dibujaron@gmail.com>
2020-11-08 21:23:08 -0500

    asdf

--- src/main/kotlin/com/dibujaron/distanthorizon/player/Player.kt
+++ src/main/kotlin/com/dibujaron/distanthorizon/player/Player.kt
@@ -96,7 +96,8 @@ class Player(val connection: WsContext) {
     private fun processClientFirstMessage(message: JSONObject) {
         val authenticationExpected = message.getBoolean("authenticated")
         if(authenticationExpected){
-            val clientKey = message.getString("client_key")
+            println(message)
+            val clientKey = message.getDouble("client_key")
             val authResultStr = URL(DHServer.authenticationUrl + "/" + clientKey).readText()
             val resultJson = JSONObject(authResultStr)
             val found = resultJson.getBoolean("found")
